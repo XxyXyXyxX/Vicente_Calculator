f014e4f11d71507253d64d88ba0f22a4fadcb309
XxyXyXyxX <jastypogi01@gmail.com>
2022-01-13 13:09:53 +0800

    New Version

--- app/src/main/java/mcm/edu/ph/vicente_calculator/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/vicente_calculator/MainActivity.java
@@ -3,12 +3,47 @@ package mcm.edu.ph.vicente_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView Answer;
+    EditText First_Variable, Second_Variable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void Calculator(View v){
+        Answer = (TextView)findViewById(R.id.Answer);
+        First_Variable = (EditText)findViewById(R.id.First_Variable);
+        Second_Variable = (EditText)findViewById(R.id.Second_Variable);
+        float Num1 = Float.parseFloat(First_Variable.getText().toString());
+        float Num2 = Float.parseFloat(Second_Variable.getText().toString());
+        float result = 0;
+        switch (v.getId())
+        {
+            case R.id.Add:
+                result = Num1 + Num2;
+                break;
+            case R.id.Subtract:
+                result = Num1 - Num2;
+                break;
+            case R.id.Multiply:
+                result = Num1 * Num2;
+                break;
+            case R.id.Divide:
+                result = Num1 / Num2;
+                break;
+            case R.id.Remainder:
+                result = Num1 % Num2;
+        }
+        Answer.setText(Float.toString(result));
+
     }
 }
